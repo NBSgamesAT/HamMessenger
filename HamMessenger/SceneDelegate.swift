161c1857ef1d615873ee3226fc859207ce98de92
NBSgamesAT <nicolas@wbsfilm.at>
2020-04-25 01:29:43 +0200

    Fixed some bugs causing crashes splitcontroller problems

--- HamMessenger/SceneDelegate.swift
+++ HamMessenger/SceneDelegate.swift
@@ -86,8 +86,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate, UISplitViewControllerDe
 
   func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController:UIViewController, onto primaryViewController:UIViewController) -> Bool {
       guard let secondaryAsNavController = secondaryViewController as? UINavigationController else { return false }
-      guard let topAsDetailController = secondaryAsNavController.topViewController as? DetailViewController else { return false }
-      if topAsDetailController.detailItem == nil {
+      guard let topAsDetailController = secondaryAsNavController.topViewController as? PrivateMessageController else { return false }
+      if topAsDetailController.currentSelectedCall == nil || topAsDetailController.currentSelectedCall == "" {
           // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
           return true
       }
